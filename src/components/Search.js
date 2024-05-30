41ff095950db3cfd7e30974de81e41bfc1f3bfd1
KAVINJOSE <kavinjose@student.tce.edu>
2024-05-30 18:26:22 +0530

    update project

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+function Search() {
+    return (
+      <div>
+        <div class="searchbar">
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+    );
+  }
+
+  export default Search
